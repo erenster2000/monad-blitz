dac271449d5c1da14a080b92967ce6818f19b99a
eren <erenster2000@gmail.com>
2026-02-28 13:24:56 +0300

    push1

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './home.css'
+
+const Home = () => {
+  return (
+    <div className={S.Container}>
+        Home
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/home.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const Container = style({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    color: 'white',
+    backgroundColor: 'black',
+});
